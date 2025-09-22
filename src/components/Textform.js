1186431fed630ca14ae280492459a35a61207d6f
Vishnu <vishu19042005@gmail.com>
2025-09-22 09:52:03 +0530

    final app

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,56 +1,65 @@
-import React,{useState} from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react'
 
-export default function Textform(props) {
-const [text, setText] = useState("this is my text");
 
-const data=()=>
-{
-    console.log("log out from the data")
-}
-const handleupclick=(props)=>{
-    // console.log("uppercase")
-    let newtext=text.toUpperCase()
-    setText(newtext)
-}
-const handlelwclick=(props)=>{
-    // console.log("uppercase")
-    let newtext=text.toLowerCase()
-    setText(newtext)
-}
-const handleonchange=(event)=>{
-    console.log("onchange")
-    setText(event.target.value)
-}
-//   setText("this i sthe 22222")
-  return (
-    <>
-    <div className="form-floating m-4">
-      <h1>{props.heading}</h1>
-      <textarea
-        className="form-control"
-        id="floatingTextarea2"
-        style={{ height: "100px" }}
-      value={text} onChange={handleonchange}></textarea>
-      <button class="btn btn-primary mt-3 m-5" onClick={handleupclick}>convert to uppercase</button>
-      <button class="btn btn-primary  mt-3 m-5" onClick={handlelwclick}>convert to lowercase</button>
-    </div>
-    <div>
-      <h1>your text summary</h1>
-      <p>{text.split(" ").length} word and {text.length} charectors
-      </p>
-      <p>time will take to read is:{0.008*text.split(" ").length}</p>
-      <h2>Preview</h2>
-      <p>{text}</p>
-    </div>
-    </>
-  );
-}
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
 
-Textform.propTypes = {
-  heading: PropTypes.string, // remove isRequired
-};
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
 
-Textform.defaultProps = {
-  heading:"enter your text here:="
-};
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+
+    // Credits: A
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
+    // Credits: Coding Wala
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+            <h1 className='mb-4'>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        </div>
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
+        </>
+    )
+}
